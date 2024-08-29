a48d9da7609a2a8b0d820b484b5c2074bd4d9924
Viieyeon <moonanita15@gmail.com>
2024-08-29 17:14:29 +0600

    feat: Добавление нового компонента элемента списка Добавление методов удаление и добавления

--- todos/src/app/components/to-do-list/to-do-list.component.ts
+++ todos/src/app/components/to-do-list/to-do-list.component.ts
@@ -1,12 +1,44 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ToDoListItemComponent } from '../to-do-list-item/to-do-list-item.component';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-to-do-list',
   standalone: true,
-  imports: [],
+  imports: [NgFor, FormsModule, ToDoListItemComponent, MatInputModule],
   templateUrl: './to-do-list.component.html',
   styleUrl: './to-do-list.component.scss'
 })
 export class ToDoListComponent {
 
+  inputValue = '';
+  todoArray = [
+    {
+      id: 1,
+      text: "Todo1"
+    },
+    {
+      id: 2,
+      text: "Todo2"
+    },
+    {
+      id: 3,
+      text: "Todo3"
+    },
+  ]
+
+
+  deleteTodo(id: number){
+    let newArray = this.todoArray.filter(item => item.id !== id );
+    this.todoArray = [...newArray];
+  }
+
+  addTodo(){
+    let maxId = Math.max(...this.todoArray.map(item => item.id), 0);
+    let newTodo = {id: maxId + 1, text: this.inputValue}
+    this.todoArray = [...this.todoArray, newTodo];
+    this.inputValue = '';
+  }
 }
