59d94acb29c313db08be3c3a085c6fc3899cd39e
Viieyeon <moonanita15@gmail.com>
2024-09-09 17:24:27 +0600

    feat: добавлен компонент кнопки и прогресс-спиннер

--- todos/src/app/components/to-do-list/to-do-list.component.ts
+++ todos/src/app/components/to-do-list/to-do-list.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-to-do-list',
   standalone: true,
-  imports: [],
+  imports: [MatProgressSpinnerModule, ButtonComponent],
   templateUrl: './to-do-list.component.html',
   styleUrl: './to-do-list.component.scss'
 })
 export class ToDoListComponent {
+  isLoading: boolean = true;
 
+  ngOnInit(){
+    setTimeout(()=> {
+      this.isLoading = false;
+    }, 500)
+  }
 }
